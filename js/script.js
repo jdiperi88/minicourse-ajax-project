32b6b546a5c7553f6beb3bc0c5b8748e4ab108a6
jdiperi88 <jdiperi88@yahoo.com>
2018-07-22 00:26:04 -0400

    updated readme.md file

--- js/script.js
+++ js/script.js
@@ -18,12 +18,23 @@ function loadData() {
     var address = streetStr + ',' + cityStr;
 
     $greeting.text('So, you want to live at '+ address + '?')
-
     var streetViewURL = `http://maps.googleapis.com/maps/api/streetview?size=600x300&location=${address}`;
     $body.append(`<img class="bgimg" src="${streetViewURL}" >`)
-
     // YOUR CODE GOES HERE!
-
+    var nytimesUrl = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${cityStr}&sort=newest&api-key=48a413a76aac4948a321914cc53c9692`
+    $.getJSON(nytimesUrl, function(data) {
+        console.log(data)
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr.toUpperCase())
+        var articles  = data.response.docs;
+        articles.forEach(i => {
+            $nytElem.append(
+                `<li class="article"> 
+                    <a href="${i.web_url}"> ${i.headline.main}</a>
+                    <p>${i.snippet}</p>
+                </li>`)
+            
+        });
+    })
     return false;
 };
 
---
+++ readme.md
@@ -0,0 +1,9 @@
+#Udacity Ajax Course
+
+## How to Run
+* git clone the repo
+* run the index.html file in the brower
+
+## About
+* This course is just to get students more comfortable with asynchronous javascript calls.
+* It uses the google api as an example.
